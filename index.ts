7bc6211f118b65b09700f8db8396d79ab107032a
Tobi Okanlawon <tobiinlondon34@gmail.com>
2026-01-31 00:26:26 +0100

    Add two commands: add, list; name utility tango
    I picked tango because that's what came to mind.
    
    But also, a tango is a consumable in dota 2 that heals you slowly. I
    think that this project is a step in that direction

--- index.ts
+++ index.ts
@@ -35,33 +35,79 @@ const Controller = (conn: Database) => {
     addTodo: (todo: string) => {
       // adds a new todo to the database
 
-      if(todo == "") throw new Error("todo cannot be empty")
+      if (todo == "") throw new Error("todo cannot be empty");
 
-      const sql = 'INSERT INTO todos (name) VALUES ($name)'
-      
-      const result = conn.query(sql).as(Todo)
+      const sql = "INSERT INTO todos (name) VALUES ($name)";
 
-      const todoResult = result.get(todo)
-      
-      return todoResult;
-    },
-    
-    listTodo: () => {
-      const sql = 'SELECT * FROM todos'
       const result = conn.query(sql).as(Todo);
 
-      const todos = result.all()
+      const todoResult = result.get(todo);
+
+      return todoResult;
+    },
+
+    listTodo: () => {
+      const sql = "SELECT * FROM todos";
+      const result = conn.query(sql).as(Todo);
+
+      const todos = result.all();
+
       return todos;
     },
-    
+
     deleteTodo: (id: number) => {
-      const sql = 'DELETE FROM todos WHERE id = $id'
+      const sql = "DELETE FROM todos WHERE id = $id";
       const query = conn.query(sql);
 
-      query.get(id)
+      query.get(id);
       return;
     },
-  }
-}
+  };
+};
 
-const c = Controller(db)
+const c = Controller(db);
+
+// next is to take the commands from the command line and link them to the controller methods
+
+const formatTodoArray = (arrOfTodos: Todo[]) => {
+  let returnString = "";
+  for (let todo of arrOfTodos) {
+    returnString += todo.toString();
+    returnString += "\n";
+  }
+
+  return returnString;
+};
+
+const program = new Command();
+
+program
+  .name("tango")
+  .description(
+    "tango is a todo list application written to join with a larger host of personalized self-organization software",
+  )
+  .version(packageJSON.version);
+
+program
+  .command("add <todo>")
+  .description("add a single todo to the list")
+  .action((todo) => {
+    try {
+      c.addTodo(todo);
+    } catch (e) {
+      console.error("An error occurred while trying to add todo", e);
+    }
+  });
+
+program
+  .command("list")
+  .description("get the todos in the system")
+  .action(() => {
+    try {
+      console.log(formatTodoArray(c.listTodo()));
+    } catch (e) {
+      console.error("An error occurred while trying to list todos");
+    }
+  });
+
+program.parse(process.argv);
